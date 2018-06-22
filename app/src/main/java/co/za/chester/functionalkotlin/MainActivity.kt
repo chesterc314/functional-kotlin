d8061e89ee9c8ef45edd9b1a561e7290f635bb88
Chester Cobus <cacobus15@gmail.com>
2018-06-22 22:16:02 +0200

    master function-kotlin

---
+++ app/src/main/java/co/za/chester/functionalkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.za.chester.functionalkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/co/za/chester/functionalkotlin/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package co.za.chester.functionalkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import gr.net.maroulis.library.EasySplashScreen
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val easySplashScreenView: View = EasySplashScreen(this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity::class.java)
+                .withSplashTimeOut(5000)
+                .withBackgroundResource(android.R.color.white)
+                .withLogo(R.mipmap.lambda_luminaries)
+                .create()
+
+        setContentView(easySplashScreenView)
+    }
+}
