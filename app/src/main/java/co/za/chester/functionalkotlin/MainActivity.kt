87c5c30c11ec6342ec53a31d6edc2e67f3caa67f
Chester Cobus <cacobus15@gmail.com>
2018-06-23 00:35:50 +0200

    master: implementation for the presentation with content refactoring

--- app/src/main/java/co/za/chester/functionalkotlin/MainActivity.kt
+++ app/src/main/java/co/za/chester/functionalkotlin/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
 import org.funktionale.option.Option
 
 data class Slide(val title: String, val sections: Set<Section>)
-data class Section(val content: String, val actions: Option<() -> Unit> = Option.None)
+data class Section(val content: String, val maybeAction: Option<(String) -> String> = Option.None)
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,18 +17,18 @@ class MainActivity : AppCompatActivity() {
             Slide("Why Kotlin?", setOf(Section(resources.getString(R.string.why_kotlin_content)))),
             Slide("Functional Kotlin aspects", setOf(Section(resources.getString(R.string.functional_kotlin_aspects)))),
             Slide("Immutability", setOf(
-                    Section(resources.getString(R.string.immutability_content), Option.Some({
-
+                    Section(resources.getString(R.string.immutability_content), Option.Some({ it ->
+                        it.replace("{result1}","")
                     }))
             )),
             Slide("Higher-Order Functions", setOf(
-                    Section(resources.getString(R.string.high_order_functions_content), Option.Some({
-
+                    Section(resources.getString(R.string.high_order_functions_content), Option.Some({ it ->
+                        it.replace("{result1}","")
                     }))
             )),
             Slide("FunKTionale", setOf(
-                    Section(resources.getString(R.string.funKTionale_content), Option.Some({
-
+                    Section(resources.getString(R.string.funKTionale_content), Option.Some({ it ->
+                        it.replace("{result1}","")
                     }))
             ))
     )
@@ -43,7 +43,10 @@ class MainActivity : AppCompatActivity() {
     private fun updateTitleAndContent(currentSlide: Slide) {
         title = currentSlide.title
         val textView: TextView = findViewById(R.id.textView)
-        textView.text = currentSlide.sections.fold("", { acc, it -> acc + it.content })
+        val content = currentSlide.sections.fold("", { acc, it ->
+            "$acc${it.maybeAction.map { action -> action(it.content) }}"
+        })
+        textView.text = content
     }
 
     fun next(view: View) {
