711f2834f2d290958bfcabb30ca670927d2d51ef
Chester Cobus <cacobus15@gmail.com>
2018-06-23 23:57:27 +0200

    master: the slides move next and back, added mutability for count

--- app/src/main/java/co/za/chester/functionalkotlin/MainActivity.kt
+++ app/src/main/java/co/za/chester/functionalkotlin/MainActivity.kt
@@ -5,37 +5,39 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.TextView
 import org.funktionale.option.Option
+import org.funktionale.option.getOrElse
 
 data class Slide(val title: String, val sections: Set<Section>)
-data class Section(val content: String, val maybeAction: Option<(String) -> String> = Option.None)
+data class Section(val content: String, val maybeAction: Option<(String) -> String> = Option.Some({ it -> it}))
 
 class MainActivity : AppCompatActivity() {
-
-    private val slides: Set<Slide> = setOf(
-            Slide("About Me", setOf(Section(resources.getString(R.string.about_me_content)))),
-            Slide("What is Kotlin?", setOf(Section(resources.getString(R.string.what_is_kotlin_content)))),
-            Slide("Why Kotlin?", setOf(Section(resources.getString(R.string.why_kotlin_content)))),
-            Slide("Functional Kotlin aspects", setOf(Section(resources.getString(R.string.functional_kotlin_aspects)))),
-            Slide("Immutability", setOf(
-                    Section(resources.getString(R.string.immutability_content), Option.Some({ it ->
-                        it.replace("{result1}","")
-                    }))
-            )),
-            Slide("Higher-Order Functions", setOf(
-                    Section(resources.getString(R.string.high_order_functions_content), Option.Some({ it ->
-                        it.replace("{result1}","")
-                    }))
-            )),
-            Slide("FunKTionale", setOf(
-                    Section(resources.getString(R.string.funKTionale_content), Option.Some({ it ->
-                        it.replace("{result1}","")
-                    }))
-            ))
-    )
+    private var count = 0
+    private lateinit var slides: Set<Slide>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        slides = setOf(
+                co.za.chester.functionalkotlin.Slide("About Me", kotlin.collections.setOf(co.za.chester.functionalkotlin.Section(resources.getString(co.za.chester.functionalkotlin.R.string.about_me_content)))),
+                Slide("What is Kotlin?", setOf(Section(resources.getString(R.string.what_is_kotlin_content)))),
+                Slide("Why Kotlin?", setOf(Section(resources.getString(R.string.why_kotlin_content)))),
+                Slide("Functional Kotlin aspects", setOf(Section(resources.getString(R.string.functional_kotlin_aspects)))),
+                Slide("Immutability", setOf(
+                        Section(resources.getString(R.string.immutability_content), Option.Some({ it ->
+                            it.replace("{result1}", "")
+                        }))
+                )),
+                Slide("Higher-Order Functions", setOf(
+                        Section(resources.getString(R.string.high_order_functions_content), Option.Some({ it ->
+                            it.replace("{result1}", "")
+                        }))
+                )),
+                Slide("FunKTionale", setOf(
+                        Section(resources.getString(R.string.funKTionale_content), Option.Some({ it ->
+                            it.replace("{result1}", "")
+                        }))
+                ))
+        )
         val currentSlide = slides.first()
         updateTitleAndContent(currentSlide)
     }
@@ -44,20 +46,26 @@ class MainActivity : AppCompatActivity() {
         title = currentSlide.title
         val textView: TextView = findViewById(R.id.textView)
         val content = currentSlide.sections.fold("", { acc, it ->
-            "$acc${it.maybeAction.map { action -> action(it.content) }}"
+            "$acc${it.maybeAction.map { action -> action(it.content) }.getOrElse { "" }}"
         })
         textView.text = content
     }
 
     fun next(view: View) {
-        val currentSlide = slides.first()
-        val updatedSlides = slides.minusElement(currentSlide)
-        updateTitleAndContent(updatedSlides.first())
+        if (count < slides.size - 1) {
+            updateTitleAndContent(slides.elementAt(++count))
+        } else {
+            count = slides.size - 1
+            updateTitleAndContent(slides.elementAt(count))
+        }
     }
 
     fun back(view: View) {
-        val currentSlide = slides.first()
-        val updatedSlides = slides.minusElement(currentSlide)
-        updateTitleAndContent(updatedSlides.first())
+        if (count > 0) {
+            updateTitleAndContent(slides.elementAt(--count))
+        } else {
+            count = 0
+            updateTitleAndContent(slides.elementAt(count))
+        }
     }
 }
