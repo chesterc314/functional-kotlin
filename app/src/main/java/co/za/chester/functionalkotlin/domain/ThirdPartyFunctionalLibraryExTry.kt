95229264cf26049b173c067fc51c04a8e81f59ac
Chester Cobus <cacobus15@gmail.com>
2018-11-10 19:32:55 +0200

    master: fixed the grammer in the Try slide

--- app/src/main/java/co/za/chester/functionalkotlin/domain/ThirdPartyFunctionalLibraryExTry.kt
+++ app/src/main/java/co/za/chester/functionalkotlin/domain/ThirdPartyFunctionalLibraryExTry.kt
@@ -32,7 +32,7 @@ object ThirdPartyFunctionalLibraryExTry {
         val tryList = Try { listOf(1, 2, 3, 4, -1) }
         val failedCondition = tryList.filter { list -> list.all { it > 0 } }
         val filterCondition = if (failedCondition.isFailure()) {
-            "You can use `filter` to cause a failure when the predicate is not satisfied, this when return `Failure`"
+            "You can use `filter` to cause a failure when the predicate is not satisfied, this will return a `Failure`"
         } else {
             ""
         }
@@ -41,7 +41,7 @@ object ThirdPartyFunctionalLibraryExTry {
         } else {
             "Failure"
         }
-        tryGetLotteryNumbers.getOrDefault { emptyList() }
+        val default: List<String> = tryGetLotteryNumbers.getOrDefault { emptyList() }
         tryGetLotteryNumbers.getOrElse { ex: Throwable ->
             when (ex) {
                 is AuthorizationException -> emptyList()
@@ -51,10 +51,10 @@ object ThirdPartyFunctionalLibraryExTry {
         return when (tryGetLotteryNumbers) {
             is Success -> "Success"
             is Failure -> "* The `Try` object is used to handle exceptions in a more graceful manner," +
-                    " when the execution is successful it returns `Success` (you can check the status of the Try `isSuccess`)" +
+                    " when the execution is successful it returns `Success` (you can check the status of the Try with`isSuccess`)" +
                     "or an exception occurs it returns `$status` (you can check the status of the Try `isFailure`)\n" +
                     "* $filterCondition\n" +
-                    "* The `getOrDefault` function allows you to set a failure when occurs \n" +
+                    "* The `getOrDefault` function allows you to set a default when a failure occurs \n" +
                     "* The `getOrElse` allows you to check for a particular exception when a failure occurs\n"
         }
     }
